ba83621482944b1bca3a56a8e8fd1b3bfb03ff34
kawasima <kawasima1016@gmail.com>
2012-08-01 19:27:57 +0900

    Added iterator to EasyApiException.

--- src/main/java/net/unit8/sastruts/easyapi/EasyApiException.java
+++ src/main/java/net/unit8/sastruts/easyapi/EasyApiException.java
@@ -1,8 +1,12 @@
 package net.unit8.sastruts.easyapi;
 
+import java.util.Iterator;
+
 @SuppressWarnings("serial")
 public class EasyApiException extends Exception {
 	private String messageCode;
+	private String message;
+
 	protected EasyApiException next;
 	protected EasyApiException prev;
 
@@ -28,5 +32,30 @@ public class EasyApiException extends Exception {
 			first = first.prev;
 		}
 		return first;
+	}
+
+	public Iterator<EasyApiException> iterator() {
+		final EasyApiException current = first();
+		return new Iterator<EasyApiException>() {
+			public boolean hasNext() {
+				return false;
+			}
+
+			public EasyApiException next() {
+				return null;
+			}
+
+			public void remove() {
+			}
+		};
+	}
+
+	public String getMessageCode() {
+		return messageCode;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
 	}
 }
